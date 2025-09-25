ce814023b0da4aa0d5bf8799ad4f9dd2c7bfc371
vivgxojo <maryse.mongeau@cegepoutaouais.qc.ca>
2025-09-25 11:54:36 -0400

    Ajout solution tests unitaries mots de passe

---
+++ tests_mots_de_passes.py
@@ -0,0 +1,25 @@
+import pytest
+from mots_passes import verifier_mot_de_passe  # adapter au nom de ton fichier
+
+def test_trop_court(capsys):
+    # Arrange
+    mdp = "abc"
+    # Act
+    result = verifier_mot_de_passe(mdp)
+    # Assert
+    assert result is False
+
+def test_sans_majuscule(capsys):
+    mdp = "abcdefg1"
+    result = verifier_mot_de_passe(mdp)
+    assert result is False
+
+def test_sans_chiffre(capsys):
+    mdp = "Abcdefgh"
+    result = verifier_mot_de_passe(mdp)
+    assert result is False
+
+def test_mot_de_passe_valide():
+    mdp = "Abcdefg1"
+    result = verifier_mot_de_passe(mdp)
+    assert result is True
