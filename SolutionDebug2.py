90f61daeec6add8eb4b7749c340db3a43379f979
vivgxojo <maryse.mongeau@cegepoutaouais.qc.ca>
2025-09-25 11:32:57 -0400

    Ajout solution version 2 : programme principal à plusieurs retraits.

--- SolutionDebug2.py
+++ SolutionDebug2.py
@@ -28,18 +28,34 @@ if __name__ == "__main__":
     print("*************************")
     print("***      GUICHET      ***")
     print("*************************")
-    print(f"Solde initial:  {solde} $")
+    print(f"Solde initial:  {solde:.2f} $")
     print("*************************")
 
-    retrait_1 = float(input("Retrait 1: Combien voulez-vous retirer? "))
-    solde = retrait(solde, retrait_1)
-
-    retrait_2 = float(input("Retrait 2: Combien voulez-vous retirer? "))
-    solde = retrait(solde, retrait_2)
+    ls_retraits = []
+    while True:
+        choix = input("Voulez-vous faire un autre retrait? (o/n) ")
+        if choix == "o":
+            while True:
+                try:
+                    retrait_1 = float(input("Retrait 1: Combien voulez-vous retirer? "))
+                    if retrait_1 <= 0 or retrait_1 > solde:
+                        print(f"Entrez un entier positif inférieur ou égal au solde de {solde}.")
+                    else:
+                        break
+                except ValueError:
+                    print("Entrée invalide : entrez un entier (ex. 2).")
+            solde = retrait(solde, retrait_1)
+            ls_retraits.append(retrait_1)
+        elif choix == "n":
+            break
+        else:
+            print("Choix non valide, entrez la lettre o pour oui ou n pour non.")
 
     print("*********************")
-    print(f"Solde initial: {MONTANT_INITIAL} $")
-    print(f"Retrait 1:     {retrait_1} $")
-    print(f"Retrait 2:     {retrait_2} $")
-    print(f"Solde final:   {solde} $")
+    print(f"Solde initial: {MONTANT_INITIAL:.2f} $")
+
+    for i in range(len(ls_retraits)):
+        print(f"Retrait {i+1}:     {ls_retraits[i]:.2f} $")
+
+    print(f"Solde final:   {solde:.2f} $")
     print("*********************")
